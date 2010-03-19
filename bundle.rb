b2d8bc85842a8e74266c35290c71865c44c9bb07
Andrew Shebanow <ashebanow@gmail.com>
2010-03-19 13:57:34 -0700

    update bundle syntax for new naming rules

--- bundle.rb
+++ bundle.rb
@@ -1,8 +1,9 @@
 require 'ruble'
 
-bundle 'YAML' do |bundle|
+bundle do |bundle|
   bundle.author = 'Benjamin Jackson'
   bundle.contact_email_rot_13 = 'ora@vapbzhzqrfvta.pbz'
+  bundle.display_name = "YAML"
   bundle.description =  <<END
 <a href="http://www.yaml.org/">YAML</a> is a straightforward machine parsable data serialization format designed for human readability and interaction with scripting languages such as Perl and Python.
 END
