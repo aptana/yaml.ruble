ae9c1f7ab7e1e9141d14214c2a8242dec19f18ea
Ingo Muschenetz <ingo@muschenetz.com>
2011-07-05 16:09:53 -0700

    Changed template definition to remove reference to non-existant lib file. Fixes issue APSTUD-2842.

--- templates/templates.rb
+++ templates/templates.rb
@@ -4,7 +4,7 @@ template "YAML Template" do |t|
   t.filetype = "*.yaml"
   t.invoke do |context|
     ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.yaml")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_PATH']}/templates/template.yaml")
     raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
  end
 end
