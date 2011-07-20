1585b0b1416ba9243f0ef5df03e58e89f4e16c22
Allen Yeung <ayeung@aptana.com>
2011-07-20 15:11:36 -0700

    Fix APSTUD-2134:Add .location property to file templates

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,9 +2,5 @@ require 'ruble'
 
 template "YAML Template" do |t|
   t.filetype = "*.yaml"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_PATH']}/templates/template.yaml")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
- end
+  t.location = "templates/template.yaml"
 end
