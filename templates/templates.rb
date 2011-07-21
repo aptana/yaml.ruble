3143c55ce8d48110ff0ba19cd9f3df93456c326e
Allen Yeung <ayeung@aptana.com>
2011-07-21 14:37:39 -0700

    Revert "Fix APSTUD-2134:Add .location property to file templates"
    This reverts commit 1585b0b1416ba9243f0ef5df03e58e89f4e16c22.

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,5 +2,9 @@ require 'ruble'
 
 template "YAML Template" do |t|
   t.filetype = "*.yaml"
-  t.location = "templates/template.yaml"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_PATH']}/templates/template.yaml")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
