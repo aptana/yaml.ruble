f8964f6ad1cb67e982947441e597f5ec1b983137
mxia <mxia@aptana.com>
2011-07-29 15:21:01 -0700

    Commented out the template definition until it has some content (related to APSTUD-3106).

--- templates/templates.rb
+++ templates/templates.rb
@@ -1,6 +1,7 @@
 require 'ruble'
 
-template "YAML Template" do |t|
-  t.filetype = "*.yaml"
-  t.location = "templates/template.yaml"
-end
+# Commenting out template code until we have actual default content to use
+#template "YAML Template" do |t|
+#  t.filetype = "*.yaml"
+#  t.location = "templates/template.yaml"
+#end
